69b33ed23ef40f059506a54c61b396187dd54994
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2024-03-12 22:11:45 +0200

    Add error handling middleware and custom error classes

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,54 +1,64 @@
 import Job from "./../models/JobModel.js";
 import catchAsync from "./../utils/catchAsync.js";
+import { StatusCodes } from "http-status-codes";
+import { NotFoundError } from "./../errors/customErrors.js";
 
-export const getAllJobs = catchAsync(async (req, res) => {
+export const getAllJobs = async (req, res) => {
   const jobs = await Job.find();
-  res.status(200).json({
+  res.status(StatusCodes.OK).json({
     status: "success",
     results: jobs.length,
     data: {
       jobs,
     },
   });
-});
+};
 
-export const getJob = catchAsync(async (req, res) => {
+export const getJob = async (req, res) => {
   const job = await Job.findById(req.params.id);
+  if (!job) throw new NotFoundError(`No job with that id ${req.params.id}`);
+
   res.status(200).json({
     status: "success",
-    results: job.length,
     data: {
       job,
     },
   });
-});
+};
 
-export const createJob = catchAsync(async (req, res) => {
+export const createJob = async (req, res) => {
   const newJob = await Job.create(req.body);
-  res.status(201).json({
+
+  res.status(StatusCodes.CREATED).json({
     status: "success",
     data: {
       newJob,
     },
   });
-});
+};
 
-export const updateJob = catchAsync(async (req, res) => {
+export const updateJob = async (req, res) => {
   const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
-  res.status(200).json({
+
+  if (!updateJob)
+    throw new NotFoundError(`No job with that id ${req.params.id}`);
+
+  res.status(StatusCodes.OK).json({
     status: "success",
     data: {
       updatedJob,
     },
   });
-});
+};
 
 export const deleteJob = catchAsync(async (req, res) => {
-  await Job.findByIdAndDelete(req.params.id);
-  res.status(204).json({
+  const deletedJob = await Job.findByIdAndDelete(req.params.id);
+  if (!deletedJob)
+    throw new NotFoundError(`No job with that id ${req.params.id}`);
+  res.status(StatusCodes.OK).json({
     status: "success",
     data: null,
   });
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+import "express-async-errors";
 import express from "express";
 import * as dotenv from "dotenv";
 import morgan from "morgan";
 import mongoose from "mongoose";
-import jobRouter from "./routes/jobRouter.js";
 
+import jobRouter from "./routes/jobRouter.js";
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 const app = express();
 dotenv.config();
 
@@ -14,6 +16,13 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 
 app.use("/api/v1/jobs", jobRouter);
+
+app.use("*", (req, res, next) => {
+  res
+    .status(404)
+    .json({ status: "fail", message: "this route not found on the server" });
+});
+app.use(errorHandlerMiddleware);
 
 const DB = process.env.DATABASE.replace(
   "<password>",
