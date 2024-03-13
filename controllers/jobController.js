2c0486f60a5b48b6a7ddd20c4ec04f4622a49b10
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2024-03-13 12:48:12 +0200

    Refactor job validation and add ID parameter validation

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -16,8 +16,6 @@ export const getAllJobs = async (req, res) => {
 
 export const getJob = async (req, res) => {
   const job = await Job.findById(req.params.id);
-  if (!job) throw new NotFoundError(`No job with that id ${req.params.id}`);
-
   res.status(200).json({
     status: "success",
     data: {
@@ -28,7 +26,6 @@ export const getJob = async (req, res) => {
 
 export const createJob = async (req, res) => {
   const newJob = await Job.create(req.body);
-
   res.status(StatusCodes.CREATED).json({
     status: "success",
     data: {
@@ -43,9 +40,6 @@ export const updateJob = async (req, res) => {
     runValidators: true,
   });
 
-  if (!updateJob)
-    throw new NotFoundError(`No job with that id ${req.params.id}`);
-
   res.status(StatusCodes.OK).json({
     status: "success",
     data: {
@@ -55,9 +49,7 @@ export const updateJob = async (req, res) => {
 };
 
 export const deleteJob = catchAsync(async (req, res) => {
-  const deletedJob = await Job.findByIdAndDelete(req.params.id);
-  if (!deletedJob)
-    throw new NotFoundError(`No job with that id ${req.params.id}`);
+  await Job.findByIdAndDelete(req.params.id);
   res.status(StatusCodes.OK).json({
     status: "success",
     data: null,
--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -6,11 +6,20 @@ import {
   deleteJob,
 } from "../controllers/jobController.js";
 
+import {
+  validateJobInput,
+  validateIdParam,
+} from "../middleware/validationMiddleware.js";
+
 import express from "express";
 
 const router = express.Router();
 
-router.route("/").get(getAllJobs).post(createJob);
-router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob);
+router.route("/").get(getAllJobs).post(validateJobInput, createJob);
+router
+  .route("/:id")
+  .get(validateIdParam, getJob)
+  .patch(validateJobInput, validateIdParam, updateJob)
+  .delete(validateIdParam, deleteJob);
 
 export default router;
